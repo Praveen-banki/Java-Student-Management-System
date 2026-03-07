cd5d27460fa46c389d255d014205c008bc85ec29
prawin <prawinbanki7@gmail.com>
2026-03-07 18:19:15 +0530

    update the students

--- Main.java
+++ Main.java
@@ -2,20 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         StudentService service = new StudentService();
 
-        System.out.println("Enter student id:");
-        int id = sc.nextInt();
+        for(int i=0;i<2;i++) {
 
-        System.out.println("Enter student name:");
-        String name = sc.next();
+            System.out.println("Enter student id:");
+            int id = sc.nextInt();
 
-        System.out.println("Enter student marks:");
-        int marks = sc.nextInt();
+            System.out.println("Enter student name:");
+            String name = sc.next();
 
-        Student s = new Student(id, name, marks);
-        service.addStudent(s);
+            System.out.println("Enter student marks:");
+            int marks = sc.nextInt();
+
+            Student s = new Student(id,name,marks);
+            service.addStudent(s);
+        }
 
         System.out.println("Student Details:");
         service.displayStudents();
